9820b98604930df89810e283bb46eabecf588523
Anas MAADOUR <anas.maadour@outlook.fr>
2021-10-08 07:58:53 +0200

    Test texteffect #1

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-document.body.style.backgroundColor = 'green';
+function textEffect(element_id, text) {
+	element = document.getElementById(element_id);
+	if (element.innerHTML !== '') {
+		element.innerHTML = text;
+	} else {
+		element.innerHTML = '';
+	}
+	setTimeout(() => { textEffect(element_id, text) }, 1000);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	textEffect('title', 'Bonjour le monde');
+});
